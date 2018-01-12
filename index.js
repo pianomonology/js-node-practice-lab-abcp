a61d575b321785d1df5eaebc51a87ea6e7848d19
Snel <snelson10@sfccmo.edu>
2018-01-12 06:06:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // write your code below!
 
 var name = "Shoko"
-var height = 80
+var height = "80"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height+1)
+/*console.log(height+1)*/
+console.log(parseInt(height)+1)
